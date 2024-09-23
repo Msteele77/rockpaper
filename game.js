5e72cbd3c53d14d90c6adbf9486bca073c9cef47
Macheya Steele <72512687+Msteele77@users.noreply.github.com>
2024-09-23 14:25:27 -0600

    created getHumanChoice function

--- game.js
+++ game.js
@@ -2,12 +2,16 @@ function getComputerChoice () {
     options = ["rock", "paper", "scissors"]
     result = options[Math.floor(Math.random()*options.length)]
     console.log(result);
-    
 }
+
 getComputerChoice();
 
 
 
+function getHumanChoice () {
+    var humanChoice = window.prompt("Enter your defense")
+    console.log(humanChoice);
+}
 
-
+getHumanChoice();
 
