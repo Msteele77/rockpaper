9d49bb45679bcf9759a81727aa3d6a2dd86cf152
Macheya Steele <72512687+Msteele77@users.noreply.github.com>
2024-09-23 14:52:07 -0600

    crated variables for humanScore and computerScore, & created playRound function

--- game.js
+++ game.js
@@ -1,17 +1,31 @@
+var humanScore = 0;
+var computerScore = 0;
+
+
+//randomly selects a choice for computer
 function getComputerChoice () { 
     options = ["rock", "paper", "scissors"]
-    result = options[Math.floor(Math.random()*options.length)]
-    console.log(result);
+    computerChoice = options[Math.floor(Math.random()*options.length)]
+    console.log(computerChoice);
 }
 
 getComputerChoice();
 
 
-
+//prompts user to enter their choice
 function getHumanChoice () {
-    var humanChoice = window.prompt("Enter your defense")
+    humanChoice = prompt("Enter your choice")
     console.log(humanChoice);
 }
 
 getHumanChoice();
+
+//takes the computer choice and human choice
+function playRound (computerSelection, humanSelection) {
+    console.log(computerSelection, humanSelection)
+    }
+    
+playRound(computerChoice, humanChoice);
+    
+
 
