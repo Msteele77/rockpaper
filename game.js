f72807812f28d24fa332a749c0494faa74e85339
Macheya Steele <72512687+Msteele77@users.noreply.github.com>
2024-09-23 14:18:36 -0600

    set up html and javascript file.created getComputerChoice function

---
+++ game.js
@@ -0,0 +1,13 @@
+function getComputerChoice () { 
+    options = ["rock", "paper", "scissors"]
+    result = options[Math.floor(Math.random()*options.length)]
+    console.log(result);
+    
+}
+getComputerChoice();
+
+
+
+
+
+
